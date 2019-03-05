0a1e4fef72d074925a92e89e975de53cf8401e9c
Jakub71 <Jakubjan0208@interia.pl>
2019-03-05 15:44:29 +0100

    dodanie widoku test

--- osoby/views.py
+++ osoby/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>witaj w Django!</h1>")
+
+def test(request):
+    return HttpResponse("To tylko test")
